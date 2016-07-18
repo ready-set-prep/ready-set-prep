20b17eeedc5c3a43307247b2aebd7be77cdb4b68
Alyssa Pratt <snubly1@gmail.com>
2016-07-18 11:16:30 -0400

    merge

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    root to:  "landingpage#index"
 
-<<<<<<< HEAD
 get '/auth/:provider/callback', to: 'sessions#create'end
-=======
-   get '/aboutus'   => 'aboutus#index'
+get '/aboutus'   => 'aboutus#index'
 end
->>>>>>> ed1096bbb729618fb48bb2dd55b204e4874f501a
