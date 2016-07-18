d01cbd08aef045265f29739d4a7df2d351701c2e
Alyssa Pratt <snubly1@gmail.com>
2016-07-18 11:20:33 -0400

    oops

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    root to:  "landingpage#index"
-
-get '/auth/:provider/callback', to: 'sessions#create'end
-get '/aboutus'   => 'aboutus#index'
+  resources :landingpage
+  get '/auth/:provider/callback', to: 'sessions#create'
+  get '/aboutus'   => 'aboutus#index'
 end
