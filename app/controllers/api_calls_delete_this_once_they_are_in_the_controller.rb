19d54149a032cb7efa9ed24b4f863e3ea1aa5cd7
Alyssa Pratt <snubly1@gmail.com>
2016-07-19 11:43:57 -0400

    attempt to make starts show up has fas failed. deleting evidence of failure.

--- app/controllers/api_calls_delete_this_once_they_are_in_the_controller.rb
+++ app/controllers/api_calls_delete_this_once_they_are_in_the_controller.rb
@@ -1,23 +1,8 @@
 
 #GLASSDOOR API ------------------
-@data = HTTParty.get "http://api.glassdoor.com/api/api.htm?v=1
-&format=json
-&t.p=#{ENV['PARTNER_ID']}
-&t.k=#{ENV['GLASSDOOR_KEY']}
-&action=employers
-&q=#{params[:q]}
-&userip=192.168.43.42
-&useragent=Chrome/%2F4.0"
 #this works if you replace the id and key with the ones in the yml file. I don't know why it does not work with env.
 
-#info of company
-@data["response"]["employers"].first["name"] #name of company
-@data["response"]["employers"].first["industry"] #industry of company
-
-#ratings of company
-@data["response"]["employers"].first["overallRating"] #rating of company
-@data["response"]["employers"].first["workLifeBalanceRating"] #workLifeBalanceRating
-@data["response"]["employers"].first["compensationAndBenefitsRating"]
+#inf
 
 #GOOGLE places API call------------
 place = place.strip.gsub(/\s/,'+') #this makes it so it can be used in the HTML.
