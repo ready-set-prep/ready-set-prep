86f4be535971092acdb2cd6c0903b162084adb0e
Spoonflower <mhowell@spoonflower.com>
2016-07-21 17:14:01 -0400

    fixing some issues

--- db/schema.rb
+++ db/schema.rb
@@ -66,8 +66,6 @@ ActiveRecord::Schema.define(version: 20160721190059) do
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
-    t.boolean  "admin"
-    t.string   "amazon_key"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
