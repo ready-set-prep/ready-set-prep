b3ef166f9e3059bb936cb06859c063bf7bf233f1
Alyssa Pratt <snubly1@gmail.com>
2016-07-21 14:44:24 -0400

    map is added

--- app/controllers/api_calls_delete_this_once_they_are_in_the_controller.rb
+++ app/controllers/api_calls_delete_this_once_they_are_in_the_controller.rb
@@ -9,12 +9,5 @@ place = place.strip.gsub(/\s/,'+') #this makes it so it can be used in the HTML.
 #current location finder? is there anything else that does this that does not use swift or C??
 #https://developers.google.com/places/ios-api/current-place google knows the answer!
 
-<iframe
-  width="600"
-  height="450"
-  src="https://www.google.com/maps/embed/v1/directions?key=KEYGOESHERE
-  &origin=Oslo+Norway
-  &destination=Telemark+Norway
-  &avoid=tolls|highways" allowfullscreen>
-</iframe>
+
 #need to replace the origin and destination with user params.
