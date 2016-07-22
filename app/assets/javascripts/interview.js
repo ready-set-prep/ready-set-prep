93d587747a2f3e98f74fc1f240724f10fefd789f
Rhonda Goolsby <rlgoolsby@gmail.com>
2016-07-22 15:05:26 -0400

    Add edits to JS

--- app/assets/javascripts/interview.js
+++ app/assets/javascripts/interview.js
@@ -1,1 +1,10 @@
-
+// window.onload = function() {
+//
+//   var startPos;
+//   var geoSuccess = function(position) {
+//     startPos = position;
+//     document.getElementById('startLat').innerHTML = startPos.coords.latitude;
+//     document.getElementById('startLon').innerHTML = startPos.coords.longitude;
+//   };
+//   navigator.geolocation.getCurrentPosition(geoSuccess);
+// };
