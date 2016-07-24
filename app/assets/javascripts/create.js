39e68ebce89bdd8c759276a195319e65973bc53e
Alyssa Pratt <snubly1@gmail.com>
2016-07-24 15:16:45 -0400

    map api gives directions to place from users current location

---
+++ app/assets/javascripts/create.js
@@ -0,0 +1,37 @@
+function initMap() {
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+  var directionsService = new google.maps.DirectionsService;
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 15
+  });
+  directionsDisplay.setMap(map);
+  var infoWindow = new google.maps.InfoWindow({map: map});
+
+  // geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      calculateAndDisplayRoute(directionsService, directionsDisplay);
+      document.getElementById('mode').addEventListener('change', function() {
+        calculateAndDisplayRoute(directionsService, directionsDisplay);
+      });
+      function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+        var selectedMode = document.getElementById('mode').value;
+        directionsService.route({
+          origin: {lat: pos.lat, lng: pos.lng},
+          destination: {lat: 35.994, lng: -78.898},
+          travelMode: google.maps.TravelMode[selectedMode]
+        }, function(response, status) {
+          if (status == google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(response);
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
+        });
+      }
+    });}}
