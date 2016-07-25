24bc29ad91b4995ea18594da31372af66e748280
Spoonflower <mhowell@spoonflower.com>
2016-07-25 11:34:49 -0400

    pintrest API and pulling from boards

---
+++ app/services/search.rb
@@ -0,0 +1,11 @@
+class Search
+  attr_reader :url, :note, :link, :id, :image
+  def initialize(result)
+    @url = result["url"]
+    @note = result["note"]
+    @link = result["link"]
+    @id = result["id"]
+    @image = result["image"].first[1]["url"]
+  end
+
+end
