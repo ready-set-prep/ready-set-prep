2cf3d40cae62ee920b2bdee927fd07d5a5ef7cb1
Alyssa Pratt <snubly1@gmail.com>
2016-07-26 14:51:50 -0400

    made seeds with the data that we need and added them to a database.

--- app/controllers/interview_controller.rb
+++ app/controllers/interview_controller.rb
@@ -8,14 +8,17 @@ class InterviewController < ApplicationController
       glassdoor_data
       @items = Amazonclass.new(@amazonparams).search
       @pins = Pintrestclass.new.search
+      @category = PositionCategory.find_by(position: params[:positions]).category
     elsif params[:positions] == ""
       @amazonparams = params[:positions].strip.gsub(/\s/,'+') #position #amazon
       @pins = Pintrestclass.new.search
+      @category = PositionCategory.find_by(position: params[:positions]).category
       glassdoor_api_call
       glassdoor_data
     else
       @items = Amazonclass.new(@amazonparams).search
       @pins = Pintrestclass.new.search
+      @category = PositionCategory.find_by(position: params[:positions]).category
     end
   end
 
