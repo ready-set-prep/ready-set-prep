988e9fd20d66429cc4e6eb7dc7bbd90e7f7bfac8
Spoonflower <mhowell@spoonflower.com>
2016-07-27 11:49:05 -0400

    getting params from saved button

--- app/controllers/companysaved_controller.rb
+++
@@ -1,3 +0,0 @@
-class CompanysavedController < ApplicationController
-
-end  
---
+++ app/controllers/savedinterviews_controller.rb
@@ -0,0 +1,19 @@
+class SavedinterviewsController < ApplicationController
+  def create
+    r = CompanySaved.new(
+      user_id: current_user.id,
+      company: params[:company],
+      position_categories: params[:position]
+      )
+        # authorize r
+        r.save!
+  end
+
+    def destroy
+      if CompanySaved.where(id: params[:id])
+        r = CompanySaved.where(id: params[:id])
+        # authorize r
+        r.first.destroy
+      end
+    end
+end
