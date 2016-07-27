f7de74e94924f0b30e3ca5ab12bc5829508c677a
Rhonda Goolsby <rlgoolsby@gmail.com>
2016-07-27 16:28:43 -0400

    Add js position choices

--- app/assets/javascripts/landingpage.js
+++ app/assets/javascripts/landingpage.js
@@ -6,27 +6,22 @@ $(".positions").click(function(){
   console.log("test");
   $(".jobcat").toggle();
   });
-// $(".jobcat").animate({ scrollTop: $(".jobcat")[0].scrollHeight}, 1000);
+
 $(".jobcat").on("click", ".kinds-of", function(){
-    // event.stopPropagation();
-    // $("#chosen-catagory").get( 0 );
     console.log($(this).text());
-    $( ".chosen-catagory" ).text( $(this).val());
-    // $(".chosen-catagory").val(function(index,val){
-      return $(this).val();
+    var clone = ($(this).text());
+
+
+
+    $(".chosen-catagory").val(function(index,val){
+      event.stopPropagation();
+      return clone;
 
     });
-    // $(".kinds-of").text();
-    // console.log("wee");
-    $(this).remove();
-  });
+    console.log("yes");
 
-  // console.log(this);
-  // $("#chosen-catagory").text();//  getting close; still need to tweek //
-  // console.log('#chosen-text');
-  // $( ".kinds-of" )[ 0 ];
-  // $(".jobcat :selected").val("0");
-  // console.log("wee");
-  // $(".kinds-of")[0].selectedIndex = 0;
-// });
-//   console.log("test again");
+
+  });// end of click function  //
+
+
+});// end of ready function  //
