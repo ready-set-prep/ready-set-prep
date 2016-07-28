4c5e3f4435fd6aed733c6d2f5042ca90d2389b07
Alyssa Pratt <snubly1@gmail.com>
2016-07-28 10:08:39 -0400

    merge

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   include Pundit
-    protect_from_forgery
 
   # rescue_from NoMethodError do
   #   redirect_to :back
