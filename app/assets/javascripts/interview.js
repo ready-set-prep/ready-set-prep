d2d269fb239680f156d42c0da88014ee1f4ccbec
Rhonda Goolsby <rlgoolsby@gmail.com>
2016-07-30 17:38:08 -0400

    Add pintrest api calls

--- app/assets/javascripts/interview.js
+++ app/assets/javascripts/interview.js
@@ -1,224 +1,63 @@
-// learn jquery ajax
-// http://net.tutsplus.com/tutorials/javascript-ajax/5-ways-to-make-ajax-calls-with-jquery/
-
-// no need to specify document ready
-
-$(function(){
-  "use strict";
+console.log("something");
 
 
-  function getResults(searchStr, offset, limit) {
-
-    console.log("search box input => " + searchStr);
-    // var baseURL = "https://api.spoonflower.com/design/search?q=cars&q=dino";
-    // var baseURL ="whatever the url is"
-    var baseURL = "https://api.spoonflower.com/design/search?";
-
-    var terms = [];
-    var query = "";
-    var jsonURL = "";
-
-    // seperate search terms
-    if (searchStr) {
-      if (searchStr.indexOf(' ')) {
-        terms = searchStr.split(" ");
-      } else {
-        terms.push(searchStr);
-      }
-    }
-
-    // build query string
-    $.each(terms, function(i) {
-      query += "q=" + this;
-      if (terms.length > 0 && i < terms.length - 1) {
-        query += "&";
-      }
-    });
-
-    jsonURL = baseURL + query + "&offset=" + offset + "&limit=" + limit;
-      console.log("AJAX URL => " + jsonURL);
-
-      // AJAX call using jQuery
-      $.getJSON(jsonURL)
-        .done( function(data) {
-          console.log('JSON:GET => Success!', data);
-
-          var htmlStr = "";
-
-          // wrap HTML around each result
-          $.each(data.results[0].results, function(i){
-
-            htmlStr +=
-       "<div class='design-card'>\n" +
-
-"<div class='design-card-image-wrapper'>" +
-
-       "  <img class='design-thumbnail' src='" + this.thumbnail_url + "'>\n" +
-
-"<div class='design-card-image-overlay'>"+
-"<div class='design-card-image-overlay-content'>"+
-
-       "  <div class='design-name'><strong> Name:  </strong> <br> " + truncate(this.name, 20) + "</div>\n" +
-       +
-       "  <div class='design-id'><strong>Product ID:  </strong> <br> " + this.id + "</div>\n" +
-       "\n";
-
-     htmlStr += "<div class='design-form-wrapper'>\n  " +
-                $(".design-form-wrapper:last").html() +
-                "\n</div>\n";
-
-     htmlStr += "<form class='design-form' action='/designs' accept-charset='UTF-8' data-remote='true' method='post'>\n";
-
-     htmlStr += "<div class='design-board-dropdown-wrapper'>\n  " +
-                $(".design-board-dropdown-label-wrapper:last").html() +
-                "\n</div>\n";
-
-     htmlStr += "<div class='design-board-dropdown-wrapper'>\n  " +
-                $(".design-board-dropdown-wrapper:last").html() +
-                "\n</div>\n &nbsp; \n";
-
-     htmlStr += "<div class='design-board-btn-wrapper'>\n  " +
-                $(".design-board-btn-wrapper:last").html() +
-                "\n</div>\n";
-
-     htmlStr += "<div class='design-card-add-board-msg'>Added</div>";
-
-     htmlStr += "  <div class='design-borad-hidden-spoonflower-id-wrapper'>\n" +
-                "    <input type='hidden' name='design[spoonflower_id]' id='design_spoonflower_id' value='" + this.id + "'>\n" +
-                "  </div>\n";
-
-     htmlStr += "</form>\n";
-
-htmlStr += "</div></div></div>\n";
-
-     htmlStr += "</div>\n";
-
-     $(".search-results-container").append(htmlStr);
-            htmlStr = "";
-   });
-
-    console.log(htmlStr);
-    updateScrollTrigger($(".design-card:last").prev().prev().prev().prev().prev());
-    currentOffset += 50;
-    console.log(" scroll offset => " + currentOffset);
-    pageNum ++;
-    console.log(" scroll page => " + pageNum);
-    // re-start scroll event triggering API call
-    $(document).on('scroll', loadScrollData);
-
-    // clear DOM for new results
-    // $(".resutls-container").empty();
-    // add search results to DOM
-    // $(".resutls-container").append("<div class='results-total'>\n  Results: " +
-    //                                 data.results[0].results.length + ", Total hits: " +
-    //                                 data.results[0].total_hits + "\n</div>\n");
-    // $(".search-results-container").append(htmlStr);
-})
-.fail( function() {
-  console.log('JSON:GET => Error!', data);
-});
-}
 
 
-/*
-* hide DOM elements
-*/
-function hideElement(element) {
-console.log("** function call : hideElement() => ", element);
-$(element).hide();
-}
 
-/*
-* hide DOM elements
-*/
-function showElement(element) {
-console.log("** function call : showElement() => ", element);
-$(element).show();
-}
 
-/*
-* add class name into DOM for checking scroll postion
-*/
-function addScrollTrigger(element) {
-console.log("** function call : addScrollTrigger() ** ");
-element.addClass("SCROLL_TRIGGER");
-// $('<input type="hidden" name="scroll_trigger" value="true" id="scrollTrigger">').appendTo($(".design-card"));
-}
+         $.ajax({
+            dataType: "json",
+             url: "https://api.pinterest.com/v1/boards/readysetprep/fashionable-interview-attire/pins?access_token=AWpsFBiCHYAcu15mAxj6h2h-BkWhFGSLUnqXuFFDRdlMEeBA0gAAAAA&fields=id%2Clink%2Cnote%2Curl%2Cimage%2Cboard%2Coriginal_link",
+             method: 'GET',
+           }) .done(function(json) {
+               $('.category-function-bar-items').append('<li id="fashionable">'+ json + '</li>');
 
-/*
-* remove trigger class name from DOM for checking scroll postion
-*/
-function removeScrollTrigger() {
-console.log("** function call : removeScrollTrigger() ** ");
-$(".SCROLL_TRIGGER").removeClass("SCROLL_TRIGGER");
-}
+            console.log(json);
+            console.log("thank you");
+            });//  $('input').val("");
 
-/*
-* remove, then add trigger class to different element in DOM for checking scroll
-*   postion
-*/
-function updateScrollTrigger(element) {
-console.log("** function call : updateScrollTrigger() ** ");
-removeScrollTrigger();
-addScrollTrigger(element);
-}
 
-/*
-* check DOM for position of trigger element for endless scroll
-*
-*   return: true - if trigger element is in the document view
-*           false - if trigger element is not in the document view
-*/
-function trigger(element) {
-if (element != undefined && element.offset() != undefined && pageNum < maxScrollLoads) {
-  var docViewTop = $(window).scrollTop();
-  var docViewBottom = docViewTop + $(window).height();
 
-  var elemTop = $(element).offset().top;
-  var elemBottom = elemTop + $(element).height();
+    console.log("please God");
 
-  return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-}
-return false;
-}
+           $.ajax({
+               dataType: "json",
+               url: "https://api.pinterest.com/v1/boards/readysetprep/conservative-interview-attire/pins?access_token=AWpsFBiCHYAcu15mAxj6h2h-BkWhFGSLUnqXuFFDRdlMEeBA0gAAAAA&fields=id%2Clink%2Cnote%2Curl%2Cimage%2Cboard%2Coriginal_link",
+               method: 'GET',
+             }) .done(function(json) {
+                 $('.category-function-bar-items').append('<li id="conservative">' + json + '</li>');
 
-/*
-*  format long strings to be approx. 21 characters worth of pixels
-*/
-function truncate(str, num) {
-var arrStr;
-if (str != undefined && str.length > 0 && num != NaN && num > 0) {
-  if (str.indexOf(" // ")) {
-    str = str.split(" // ")[0];
-  }
-  if (str.length > num) {
-    arrStr = str.split("");
-    return arrStr.slice(0, num).join("") + "...";
-  }
-}
-return str;
-}
+            console.log(json);
+            console.log("now");
+          });
 
-/*
-*  send notification messages to #notice element in page header
-*/
-function notify(str) {
-if (str != undefined && str.length > 0) {
-  $("#notice").empty();
-  $("#notice").prepend(str).fadeIn(600, "swing").delay(2000).fadeOut(600, "swing");
-}
-}
 
-/*
-*  hide designs after removing them in board show
-*/
 
-function deleteDesign() {
-var card = $(event.target).closest(".board-card");
-card.css("display", "none");
-}
 
-function bindDeleteDesign() {
-$(".remove").on("click", deleteDesign);
-}
-}
+
+  console.log("just maybe");
+
+         $.ajax({
+            dataType: "json",
+             url: "https://api.pinterest.com/v1/boards/readysetprep/buisness-casual-interview-attire/pins?access_token=AWpsFBiCHYAcu15mAxj6h2h-BkWhFGSLUnqXuFFDRdlMEeBA0gAAAAA&fields=id%2Clink%2Cnote%2Curl%2Cimage%2Cboard%2Coriginal_link",
+             method: 'GET',
+           }) .done(function(json) {
+               $('.category-function-bar-items').append('<li id="bizcas">' + json + '</li>');
+
+          console.log(json);
+        });
+
+
+
+
+  console.log("I know it's possible");
+
+       $.ajax({
+          dataType: "json",
+           url: "https://api.pinterest.com/v1/boards/readysetprep/casual-interview-attire/pins?access_token=AWpsFBiCHYAcu15mAxj6h2h-BkWhFGSLUnqXuFFDRdlMEeBA0gAAAAA&fields=id%2Clink%2Cnote%2Curl%2Cimage%2Cboard%2Coriginal_link",
+           method: 'GET',
+         }) .done(function(json) {
+              $('.category-function-bar-items').append('<li id="casual">' + json + '</li>');
+
+        console.log(json);
+      });
