7b9daae79ccf046e668f986a74cdcf74273bf4c2
Rhonda Goolsby <rlgoolsby@gmail.com>
2016-07-31 14:03:33 -0400

    Add show and hide nav bar

--- app/assets/javascripts/interview.js
+++ app/assets/javascripts/interview.js
@@ -1,64 +1,80 @@
-function myFunction() {
-  console.log("refresh")
-    location.reload();
-}
+$(document).ready(function(){
 
-
-
-
-         $.ajax({
-            dataType: "json",
-             url: "https://api.pinterest.com/v1/boards/readysetprep/fashionable-interview-attire/pins?access_token=AWpsFBiCHYAcu15mAxj6h2h-BkWhFGSLUnqXuFFDRdlMEeBA0gAAAAA&fields=id%2Clink%2Cnote%2Curl%2Cimage%2Cboard%2Coriginal_link",
-             method: 'GET',
-           }) .done(function(json) {
-               $('.category-function-bar-items').append('<li id="fashionable">'+ json + '</li>');
-
-            console.log(json);
-            console.log("thank you");
-            });//  $('input').val("");
-
-
-
-    console.log("please God");
-
-           $.ajax({
-               dataType: "json",
-               url: "https://api.pinterest.com/v1/boards/readysetprep/conservative-interview-attire/pins?access_token=AWpsFBiCHYAcu15mAxj6h2h-BkWhFGSLUnqXuFFDRdlMEeBA0gAAAAA&fields=id%2Clink%2Cnote%2Curl%2Cimage%2Cboard%2Coriginal_link",
-               method: 'GET',
-             }) .done(function(json) {
-                 $('.category-function-bar-items').append('<li id="conservative">' + json + '</li>');
-
-            console.log(json);
-            console.log("now");
-          });
+$(".show").on("click",function(){
+  console.log("test");
+  // $('.btn-nav-bar').addClass('animated slideInDown');
+  $(".btn-nav-bar").css("display", "block");
+  $(".show").css("display", "none");
+  $(".hide").css("display", "block");
+  });
+  $(".hide").on("click",function(){
+    console.log("again");
+    // $('.btn-nav-bar').addClass('animated slideOutUp');
+    $(".btn-nav-bar").css("display", "none");
+    $(".hide").css("display", "none");
+    $(".show").css("display", "block");
+    });
+});
 
 
 
 
 
-  console.log("just maybe");
-
-         $.ajax({
-            dataType: "json",
-             url: "https://api.pinterest.com/v1/boards/readysetprep/buisness-casual-interview-attire/pins?access_token=AWpsFBiCHYAcu15mAxj6h2h-BkWhFGSLUnqXuFFDRdlMEeBA0gAAAAA&fields=id%2Clink%2Cnote%2Curl%2Cimage%2Cboard%2Coriginal_link",
-             method: 'GET',
-           }) .done(function(json) {
-               $('.category-function-bar-items').append('<li id="bizcas">' + json + '</li>');
-
-          console.log(json);
-        });
-
-
-
-
-  console.log("I know it's possible");
-
-       $.ajax({
-          dataType: "json",
-           url: "https://api.pinterest.com/v1/boards/readysetprep/casual-interview-attire/pins?access_token=AWpsFBiCHYAcu15mAxj6h2h-BkWhFGSLUnqXuFFDRdlMEeBA0gAAAAA&fields=id%2Clink%2Cnote%2Curl%2Cimage%2Cboard%2Coriginal_link",
-           method: 'GET',
-         }) .done(function(json) {
-              $('.category-function-bar-items').append('<li id="casual">' + json + '</li>');
-
-        console.log(json);
-      });
+  //        $.ajax({
+  //           dataType: "json",
+  //            url: "https://api.pinterest.com/v1/boards/readysetprep/fashionable-interview-attire/pins?access_token=AWpsFBiCHYAcu15mAxj6h2h-BkWhFGSLUnqXuFFDRdlMEeBA0gAAAAA&fields=id%2Clink%2Cnote%2Curl%2Cimage%2Cboard%2Coriginal_link",
+  //            method: 'GET',
+  //          }) .done(function(json) {
+  //              $('.category-function-bar-items').append('<li id="fashionable">'+ json + '</li>');
+  //
+  //           console.log(json);
+  //           console.log("thank you");
+  //           });//  $('input').val("");
+  //
+  //
+  //
+  //   console.log("please God");
+  //
+  //          $.ajax({
+  //              dataType: "json",
+  //              url: "https://api.pinterest.com/v1/boards/readysetprep/conservative-interview-attire/pins?access_token=AWpsFBiCHYAcu15mAxj6h2h-BkWhFGSLUnqXuFFDRdlMEeBA0gAAAAA&fields=id%2Clink%2Cnote%2Curl%2Cimage%2Cboard%2Coriginal_link",
+  //              method: 'GET',
+  //            }) .done(function(json) {
+  //                $('.category-function-bar-items').append('<li id="conservative">' + json + '</li>');
+  //
+  //           console.log(json);
+  //           console.log("now");
+  //         });
+  //
+  //
+  //
+  //
+  //
+  // console.log("just maybe");
+  //
+  //        $.ajax({
+  //           dataType: "json",
+  //            url: "https://api.pinterest.com/v1/boards/readysetprep/buisness-casual-interview-attire/pins?access_token=AWpsFBiCHYAcu15mAxj6h2h-BkWhFGSLUnqXuFFDRdlMEeBA0gAAAAA&fields=id%2Clink%2Cnote%2Curl%2Cimage%2Cboard%2Coriginal_link",
+  //            method: 'GET',
+  //          }) .done(function(json) {
+  //              $('.category-function-bar-items').append('<li id="bizcas">' + json + '</li>');
+  //
+  //         console.log(json);
+  //       });
+  //
+  //
+  //
+  //
+  // console.log("I know it's possible");
+  //
+  //      $.ajax({
+  //         dataType: "json",
+  //          url: "https://api.pinterest.com/v1/boards/readysetprep/casual-interview-attire/pins?access_token=AWpsFBiCHYAcu15mAxj6h2h-BkWhFGSLUnqXuFFDRdlMEeBA0gAAAAA&fields=id%2Clink%2Cnote%2Curl%2Cimage%2Cboard%2Coriginal_link",
+  //          method: 'GET',
+  //        }) .done(function(json) {
+  //             $('.category-function-bar-items').append('<li id="casual">' + json + '</li>');
+  //
+  //       console.log(json);
+  //     });
+  //
+  //
