cd764975c798d8845a250f6621f1c29a1c5c5ce4
Alyssa Pratt <snubly1@gmail.com>
2016-07-31 12:55:14 -0400

    THE AJAX CALL IS WORKING AND DISPLAYING THINGS ON PAGE

--- app/assets/javascripts/interview.js
+++ app/assets/javascripts/interview.js
@@ -1,54 +1,47 @@
    $.ajax({
       dataType: "json",
-       url: "https://api.pinterest.com/v1/boards/readysetprep/fashionable-interview-attire/pins?access_token=KEYGOESHERE&fields=id%2Clink%2Cnote%2Curl%2Cimage%2Cboard%2Coriginal_link",
+       url: "https://api.pinterest.com/v1/boards/readysetprep/fashionable-interview-attire/pins?access_token=AWpsFBiCHYAcu15mAxj6h2h-BkWhFGSLUnqXuFFDRdlMEeBA0gAAAAA&fields=id%2Clink%2Cnote%2Curl%2Cimage%2Cboard%2Coriginal_link",
        method: 'GET',
      }) .done(function(json) {
          $('.category-function-bar-items').append('<li id="fashionable">'+ json + '</li>');
 
-//this eaches over the data and console.logs it. we need to do soemthing with it thouhg.
-//also I took out the key cause im going to push this so you'll need to stick it back in when you are ready to test it. :)
     $.each(json.data, function(index,item) {
-        console.log(item.note+" "+item.link+" "+item.url+""+item.image.original.url)
+        $(".pintrestStuff").append("<figure><a href=" + item.url + "><img src=" + item.image.original.url + "></img></a><figcaption>" + item.note + "</figcaption></figure>")
     });
-    //YASSSSSSSSSSSSSSSSSSSSSSSSSS
-      console.log("fashion");
       });
 
      $.ajax({
          dataType: "json",
-         url: "https://api.pinterest.com/v1/boards/readysetprep/conservative-interview-attire/pins?access_token=KEYGOESHERE&fields=id%2Clink%2Cnote%2Curl%2Cimage%2Cboard%2Coriginal_link",
+         url: "https://api.pinterest.com/v1/boards/readysetprep/conservative-interview-attire/pins?access_token=AWpsFBiCHYAcu15mAxj6h2h-BkWhFGSLUnqXuFFDRdlMEeBA0gAAAAA&fields=id%2Clink%2Cnote%2Curl%2Cimage%2Cboard%2Coriginal_link",
          method: 'GET',
        }) .done(function(json) {
            $('.category-function-bar-items').append('<li id="conservative">' + json + '</li>');
 
            $.each(json.data, function(index,item) {
-               console.log(item.note+" "+item.link+" "+item.url+""+item.image.original.url)
+             $(".pintrestStuff").append("<figure><a href=" + item.url + "><img src=" + item.image.original.url + "></img></a><figcaption>" + item.note + "</figcaption></figure>")
            });
-      console.log("consirv");
     });
 
    $.ajax({
       dataType: "json",
-       url: "https://api.pinterest.com/v1/boards/readysetprep/business-casual-interview-attire/pins?access_token=KEYGOESHERE&fields=id%2Clink%2Cnote%2Curl%2Cimage%2Cboard%2Coriginal_link",
+       url: "https://api.pinterest.com/v1/boards/readysetprep/business-casual-interview-attire/pins?access_token=AWpsFBiCHYAcu15mAxj6h2h-BkWhFGSLUnqXuFFDRdlMEeBA0gAAAAA&fields=id%2Clink%2Cnote%2Curl%2Cimage%2Cboard%2Coriginal_link",
        method: 'GET',
      }) .done(function(json) {
          $('.category-function-bar-items').append('<li id="bizcas">' + json + '</li>');
 
          $.each(json.data, function(index,item) {
-             console.log(item.note+" "+item.link+" "+item.url+""+item.image.original.url)
+           $(".pintrestStuff").append("<figure><a href=" + item.url + "><img src=" + item.image.original.url + "></img></a><figcaption>" + item.note + "</figcaption></figure>")
          });
-    console.log("bizcas");
   });
 
  $.ajax({
     dataType: "json",
-     url: "https://api.pinterest.com/v1/boards/readysetprep/casual-interview-attire/pins?access_token=KEYGOESHERE&fields=id%2Clink%2Cnote%2Curl%2Cimage%2Cboard%2Coriginal_link",
+     url: "https://api.pinterest.com/v1/boards/readysetprep/casual-interview-attire/pins?access_token=AWpsFBiCHYAcu15mAxj6h2h-BkWhFGSLUnqXuFFDRdlMEeBA0gAAAAA&fields=id%2Clink%2Cnote%2Curl%2Cimage%2Cboard%2Coriginal_link",
      method: 'GET',
    }) .done(function(json) {
         $('.category-function-bar-items').append('<li id="casual">' + json + '</li>');
 
         $.each(json.data, function(index,item) {
-            console.log(item.note+" "+item.link+" "+item.url+""+item.image.original.url)
+          $(".pintrestStuff").append("<figure><a href=" + item.url + "><img src=" + item.image.original.url + "></img></a><figcaption>" + item.note + "</figcaption></figure>")
         });
-  console.log("cas");
 });
