9efb8915d65d1169acfba77fc0132fc3b9f708b3
Alyssa Pratt <snubly1@gmail.com>
2016-07-31 14:44:45 -0400

    the buttons are clicking

--- app/assets/javascripts/interview.js
+++ app/assets/javascripts/interview.js
@@ -1,15 +1,24 @@
-   $.ajax({
-      dataType: "json",
-       url: "https://api.pinterest.com/v1/boards/readysetprep/fashionable-interview-attire/pins?access_token=AWpsFBiCHYAcu15mAxj6h2h-BkWhFGSLUnqXuFFDRdlMEeBA0gAAAAA&fields=id%2Clink%2Cnote%2Curl%2Cimage%2Cboard%2Coriginal_link",
-       method: 'GET',
-     }) .done(function(json) {
-         $('.category-function-bar-items').append('<li id="fashionable">'+ json + '</li>');
+//fashion
+$(document).ready(function(){
+$( ".fashionable").on("click", function() {
+  console.log("button 1")
+  $.ajax({
+     dataType: "json",
+      url: "https://api.pinterest.com/v1/boards/readysetprep/fashionable-interview-attire/pins?access_token=AWpsFBiCHYAcu15mAxj6h2h-BkWhFGSLUnqXuFFDRdlMEeBA0gAAAAA&fields=id%2Clink%2Cnote%2Curl%2Cimage%2Cboard%2Coriginal_link",
+      method: 'GET',
+    }) .done(function(json) {
+        $('.category-function-bar-items').append('<li id="fashionable">'+ json + '</li>');
 
-    $.each(json.data, function(index,item) {
-        $(".pintrestStuff").append("<figure><a href=" + item.url + "><img src=" + item.image.original.url + "></img></a><figcaption>" + item.note + "</figcaption></figure>")
-    });
-      });
+   $.each(json.data, function(index,item) {
+       $(".pintrestStuff").append("<figure><a href=" + item.url + "><img src=" + item.image.original.url + "></img></a><figcaption>" + item.note + "</figcaption></figure>")
+  });
+  });
+});
 
+$(".conservative").on("click", function() {
+//conservative
+
+  console.log("button 2")
      $.ajax({
          dataType: "json",
          url: "https://api.pinterest.com/v1/boards/readysetprep/conservative-interview-attire/pins?access_token=AWpsFBiCHYAcu15mAxj6h2h-BkWhFGSLUnqXuFFDRdlMEeBA0gAAAAA&fields=id%2Clink%2Cnote%2Curl%2Cimage%2Cboard%2Coriginal_link",
@@ -21,7 +30,11 @@
              $(".pintrestStuff").append("<figure><a href=" + item.url + "><img src=" + item.image.original.url + "></img></a><figcaption>" + item.note + "</figcaption></figure>")
            });
     });
+});
 
+$(".bizcas").on("click", function() {
+//business casual
+  console.log("button 3")
    $.ajax({
       dataType: "json",
        url: "https://api.pinterest.com/v1/boards/readysetprep/business-casual-interview-attire/pins?access_token=AWpsFBiCHYAcu15mAxj6h2h-BkWhFGSLUnqXuFFDRdlMEeBA0gAAAAA&fields=id%2Clink%2Cnote%2Curl%2Cimage%2Cboard%2Coriginal_link",
@@ -33,7 +46,11 @@
            $(".pintrestStuff").append("<figure><a href=" + item.url + "><img src=" + item.image.original.url + "></img></a><figcaption>" + item.note + "</figcaption></figure>")
          });
   });
+});
 
+$(".casual").on("click", function() {
+//casual
+  console.log("button 4")
  $.ajax({
     dataType: "json",
      url: "https://api.pinterest.com/v1/boards/readysetprep/casual-interview-attire/pins?access_token=AWpsFBiCHYAcu15mAxj6h2h-BkWhFGSLUnqXuFFDRdlMEeBA0gAAAAA&fields=id%2Clink%2Cnote%2Curl%2Cimage%2Cboard%2Coriginal_link",
@@ -44,4 +61,6 @@
         $.each(json.data, function(index,item) {
           $(".pintrestStuff").append("<figure><a href=" + item.url + "><img src=" + item.image.original.url + "></img></a><figcaption>" + item.note + "</figcaption></figure>")
         });
+});
+});
 });
