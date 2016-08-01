dcc800663cafb6897ac685f6d84ae63a21b306cc
Alyssa Pratt <snubly1@gmail.com>
2016-08-01 09:35:32 -0400

    made them all replace the existing one

--- app/assets/javascripts/interview.js
+++ app/assets/javascripts/interview.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 $("#search_submit").on("click", function(){
   var search = $("#search").val().trim().replace(/\s/g, '+')
   console.log(search)
-  // $(".defaultShop").hide();
+  $(".defaultShop").hide();
 
 $.ajax({
   dataType: "json",
@@ -12,7 +12,7 @@ $.ajax({
   method: "GET"
 }) .done(function(json) {
   $.each(json.products, function(index,item){
-    $(".shop").append("<figure><a href=" + item.pageUrl + "><img src=" + item.image.sizes.Best.url + "></img></a><figcaption>" + item.priceLabel + item.brandedName + item.description + "</figcaption></figure>")
+    $(".shop").append("<figure class='ajaxcalls'><a href=" + item.pageUrl + "><img src=" + item.image.sizes.Best.url + "></img></a><figcaption>" + item.priceLabel + item.brandedName + item.description + "</figcaption></figure>")
   });
 });
 });
@@ -20,6 +20,7 @@ $.ajax({
 $( ".fashionable").on("click", function() {
   console.log("button 1")
   $(".rubydiv").hide();
+  $(".ajaxcalls").hide();
   $.ajax({
      dataType: "json",
       url: "https://api.pinterest.com/v1/boards/readysetprep/fashionable-interview-attire/pins?access_token=AWpsFBiCHYAcu15mAxj6h2h-BkWhFGSLUnqXuFFDRdlMEeBA0gAAAAA&fields=id%2Clink%2Cnote%2Curl%2Cimage%2Cboard%2Coriginal_link",
@@ -29,7 +30,7 @@ $( ".fashionable").on("click", function() {
 
 
    $.each(json.data, function(index,item) {
-       $(".pintrestStuff").append("<figure><a href=" + item.url + "><img src=" + item.image.original.url + "></img></a><figcaption>" + item.note + "</figcaption></figure>")
+       $(".pintrestStuff").append("<figure class='ajaxcalls'><a href=" + item.url + "><img src=" + item.image.original.url + "></img></a><figcaption>" + item.note + "</figcaption></figure>")
   });
   });
 });
@@ -38,6 +39,7 @@ $(".conservative").on("click", function() {
 //conservative
   console.log("button 2")
   $(".rubydiv").hide();
+  $(".ajaxcalls").hide();
      $.ajax({
          dataType: "json",
          url: "https://api.pinterest.com/v1/boards/readysetprep/conservative-interview-attire/pins?access_token=AWpsFBiCHYAcu15mAxj6h2h-BkWhFGSLUnqXuFFDRdlMEeBA0gAAAAA&fields=id%2Clink%2Cnote%2Curl%2Cimage%2Cboard%2Coriginal_link",
@@ -46,7 +48,7 @@ $(".conservative").on("click", function() {
            $('.category-function-bar-items').append('<li id="conservative">' + json + '</li>');
 
            $.each(json.data, function(index,item) {
-             $(".pintrestStuff").append("<figure><a href=" + item.url + "><img src=" + item.image.original.url + "></img></a><figcaption>" + item.note + "</figcaption></figure>")
+             $(".pintrestStuff").append("<figure class='ajaxcalls'><a href=" + item.url + "><img src=" + item.image.original.url + "></img></a><figcaption>" + item.note + "</figcaption></figure>")
            });
     });
 });
@@ -55,6 +57,7 @@ $(".bizcas").on("click", function() {
 //business casual
   console.log("button 3")
   $(".rubydiv").hide();
+  $(".ajaxcalls").hide();
    $.ajax({
       dataType: "json",
        url: "https://api.pinterest.com/v1/boards/readysetprep/business-casual-interview-attire/pins?access_token=AWpsFBiCHYAcu15mAxj6h2h-BkWhFGSLUnqXuFFDRdlMEeBA0gAAAAA&fields=id%2Clink%2Cnote%2Curl%2Cimage%2Cboard%2Coriginal_link",
@@ -63,7 +66,7 @@ $(".bizcas").on("click", function() {
          $('.category-function-bar-items').append('<li id="bizcas">' + json + '</li>');
 
          $.each(json.data, function(index,item) {
-           $(".pintrestStuff").append("<figure><a href=" + item.url + "><img src=" + item.image.original.url + "></img></a><figcaption>" + item.note + "</figcaption></figure>")
+           $(".pintrestStuff").append("<figure class='ajaxcalls'><a href=" + item.url + "><img src=" + item.image.original.url + "></img></a><figcaption>" + item.note + "</figcaption></figure>")
          });
   });
 });
@@ -72,6 +75,7 @@ $(".casual").on("click", function() {
 //casual
   console.log("button 4")
   $(".rubydiv").hide();
+  $(".ajaxcalls").hide();
  $.ajax({
     dataType: "json",
      url: "https://api.pinterest.com/v1/boards/readysetprep/casual-interview-attire/pins?access_token=AWpsFBiCHYAcu15mAxj6h2h-BkWhFGSLUnqXuFFDRdlMEeBA0gAAAAA&fields=id%2Clink%2Cnote%2Curl%2Cimage%2Cboard%2Coriginal_link",
