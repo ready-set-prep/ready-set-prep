6c5afed589b59ad68b4f71046d6eeaec98e6af8b
Spoonflower <mhowell@spoonflower.com>
2016-08-02 09:32:56 -0400

    reseting tables to help Travis CI

--- db/migrate/20160716182509_create_position_categories.rb
+++
@@ -1,9 +0,0 @@
-class CreatePositionCategories < ActiveRecord::Migration[5.0]
-  def change
-    create_table :position_categories do |t|
-      t.string :position
-      t.string :category
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20160728143841_create_pintrestselections.rb
+++
@@ -1,8 +0,0 @@
-class CreatePintrestselections < ActiveRecord::Migration[5.0]
-  def change
-    create_table :pintrestselections do |t|
-
-      t.timestamps
-    end
-  end
-end
