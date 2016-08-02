94e6376f9aefd5300cd88fc11c61c7bf91916e88
Alyssa Pratt <snubly1@gmail.com>
2016-08-02 18:31:34 -0400

    merge

--- app/controllers/landingpage_controller.rb
+++ app/controllers/landingpage_controller.rb
@@ -1,7 +1,7 @@
 class LandingpageController < ApplicationController
   def index
     if current_user
-      @reviews = current_user.company_saveds[0..4]
+      @reviews = current_user.company_saveds.last(5)
     end
   end
 end
